72dc29aded6b1e390719b34d8578765585dc2a01
alex0blackwell <alex.blackwell314@gmail.com>
2020-11-20 23:50:45 -0800

    📦 NEW: Add search bar algorithm for filtering

---
+++ js/searchBar.js
@@ -0,0 +1,19 @@
+/**
+ * Filtering algorithm for finding chords.
+ */
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].innerHTML;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
